6cabfc956e4f87adb88e9e2c58b34d355adfe9bf
HeavyPiano <pakheichan2010@gmail.com>
2020-12-01 22:24:47 +0000

    finished both parts

--- Day1.py
+++ Day1.py
@@ -199,13 +199,35 @@ expense_list = [1411,
                 1902,
                 1741]
 
-three_entry = []
+matched_numbers = []
 
-for i in expense_list:
-    for j in expense_list:
-        for k in expense_list:
-            if i + j + k == 2020:
-                if i not in three_entry:
-                    three_entry.extend([i, j, k])
 
-print(three_entry[0]*three_entry[1]*three_entry[2])
+def two_numbers(li):
+    for i in li:
+        for j in li:
+            if i + j == 2020:
+                if i not in matched_numbers:
+                    return ([i, j])
+
+
+def three_numbers(li):
+    for i in li:
+        for j in li:
+            for k in li:
+                if i + j + k == 2020:
+                    if i not in matched_numbers:
+                        return ([i, j, k])
+
+
+def multiply(matched):
+    result = 1
+    for i in matched:
+        result = result * i
+    return result
+
+
+matched_numbers.extend(two_numbers(expense_list))
+print(f'1st part of problem: solution is {multiply(matched_numbers)}')
+matched_numbers = []
+matched_numbers.extend(three_numbers(expense_list))
+print(f'2nd part of problem: solution is {multiply(matched_numbers)}')
